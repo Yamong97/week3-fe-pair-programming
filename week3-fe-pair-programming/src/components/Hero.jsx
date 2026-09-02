3f87690d85d96b5df43f5b06009f82a3cd40f7b5
Yamong Phoo <yamongphoo012@gmail.com>
2026-09-02 17:36:04 +0300

    feat: complete navbar component refactoring

--- week3-fe-pair-programming/src/components/Hero.jsx
+++ week3-fe-pair-programming/src/components/Hero.jsx
@@ -1,23 +1,21 @@
-function Hero() {
-  return (
-    <>
-      {/* Hero section starts */}
-      <section className="hero" id="home">
-        <div className="hero-banner">
-          <h1>backroads app</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae explicabo
-            debitis est autem dicta.
-          </p>
-          <a href="#tours" className="btn hero-btn">
-            explore tours
-          </a>
-        </div>
-      </section>
-      {/* Hero section ends */}
-    </>
+import React from 'react';
+import styles from './Hero.module.css';
 
-  );
+const Hero = () => {
+  return (
+    <section className={styles.hero} id='home'>
+      <div className={styles['hero-banner']}>
+        <h1>backroads app</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          explicabo debitis est autem dicta.
+        </p>
+        <a href='#tours' className={`btn ${styles['hero-btn']}`}>
+          explore tours
+        </a>
+      </div>
+    </section>
+  )
 }
 
-export default Hero;
+export default Hero
